e7fbd248d5fae044cff2a046b2fc623715ad8665
Ben Johnson <monk.sound.design@gmail.com>
2023-08-21 16:56:34 -0400

    delete, put, getComments

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -48,7 +48,66 @@ router.post("/", async (req, res) => {
       res.status(201).json(newPost);
     }
   } catch (err) {
-    res.status(500).json(`${err.message}`);
+    res.status(500).json({
+      message: "There was an error while saving the post to the database",
+    });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const { title, contents } = req.body;
+  const { id } = req.params;
+  const updatedPost = await Posts.update(id, { title, contents });
+  try {
+    if (!updatedPost) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+    } else if (!title || !contents) {
+      res
+        .status(400)
+        .json({ message: "Please provide title and contents for the post" });
+    } else {
+      res.status(200).json(updatedPost);
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "The post information could not be modified" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedPost = await Posts.remove(id);
+  try {
+    if (!deletedPost) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+    } else {
+      res.status(200).json(deletedPost);
+    }
+  } catch (err) {
+    res.status(500).json({ message: "The post could not be removed" });
+  }
+});
+
+router.get("/:id/comments", async (req, res) => {
+  const { id } = req.params;
+  const postComments = await Posts.findPostComments(id);
+  try {
+    if (postComments.length < 1) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+    } else {
+      res.status(200).json(postComments);
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "The comments information could not be retrieved" });
   }
 });
 
