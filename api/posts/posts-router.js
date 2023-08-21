43892f080bc379688fe33b81c24ce8f120523630
Ben Johnson <monk.sound.design@gmail.com>
2023-08-21 16:33:18 -0400

    create new post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -16,9 +16,9 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const post = await Posts.findById(id);
   try {
-    const { id } = req.params;
-    const post = await Posts.findById(id);
     if (!post) {
       res
         .status(404)
@@ -30,6 +30,25 @@ router.get("/:id", async (req, res) => {
     res
       .status(500)
       .json({ message: "The post information could not be retrieved" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { title, contents } = req.body;
+  const newPost = await Posts.insert({
+    title,
+    contents,
+  });
+  try {
+    if (!title || !contents) {
+      res
+        .status(400)
+        .json({ message: "Please provide title and contents for the post" });
+    } else {
+      res.status(201).json(newPost);
+    }
+  } catch (err) {
+    res.status(500).json(`${err.message}`);
   }
 });
 
