b65906633d47f36b735830130832d481896a72d6
Ben Johnson <monk.sound.design@gmail.com>
2023-08-21 16:02:51 -0400

    wired server and tested router

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,15 @@
 // implement your posts router here
+const express = require("express");
+const Posts = require("./posts-model");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  try {
+    throw new Error("test");
+  } catch (err) {
+    res.status(500).json({ message: `${err.message}` });
+  }
+});
+
+module.exports = router;
